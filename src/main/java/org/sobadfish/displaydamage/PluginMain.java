ca3c3db0ce4a3e45307fc5814309bf517705a92a
Sobadfish <3098143605>
2024-08-05 11:16:04 +0800

    优化粒子显示算法 粒子显示更加自然

--- src/main/java/org/sobadfish/displaydamage/PluginMain.java
+++ src/main/java/org/sobadfish/displaydamage/PluginMain.java
@@ -16,8 +16,12 @@ import cn.nukkit.math.Vector3;
 import cn.nukkit.math.Vector3f;
 import cn.nukkit.network.protocol.SpawnParticleEffectPacket;
 import cn.nukkit.plugin.PluginBase;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.SplittableRandom;
 
 
 /**
@@ -37,8 +41,11 @@ public class PluginMain extends PluginBase implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityDamage(EntityDamageEvent event){
+        if(event.isCancelled()){
+            return;
+        }
         Entity entity = event.getEntity();
-        int damage = (int) event.getDamage();
+        int damage = (int) event.getFinalDamage();
         if(event instanceof EntityDamageByEntityEvent){
             Entity damager = ((EntityDamageByEntityEvent) event).getDamager();
 
@@ -64,15 +71,31 @@ public class PluginMain extends PluginBase implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityHealEvent(EntityRegainHealthEvent event) {
+        if(event.isCancelled()){
+            return;
+        }
         Entity entity = event.getEntity();
         int heal = (int) event.getAmount();
         displayAsParticle(heal+"","damage:ph",entity);
     }
 
+    private static final SplittableRandom RANDOM = new SplittableRandom(System.currentTimeMillis());
+
+    public static int rand(int min, int max) {
+        return min == max ? max : RANDOM.nextInt(max + 1 - min) + min;
+    }
+
     private void displayAsParticle(String damage,String numTitle, Entity displayEntity){
+        float vx,vy,vz;
+        vx = rand(0,4) / 10f;
+        vy = rand(0,4)/ 10f;
+        vz = rand(0,4)/ 10f;
+        Gson gs = new Gson();
+
         for(Player ckPlayer: displayEntity.level.getChunkPlayers(displayEntity.getChunkX(),displayEntity.getChunkZ()).values()){
             float v2 = 0.25f;
-            Vector3f dv3 = displayEntity.asVector3f().add(0,1.5f);
+            Vector3f dv3 = displayEntity.asVector3f().add(0,2f);
+
             for(char c: damage.toCharArray()){
                 if(c >= '0' && c <= '9'){
                     Vector3f r2 = getSide(dv3,ckPlayer.getDirection().rotateY(),v2);
@@ -81,10 +104,17 @@ public class PluginMain extends PluginBase implements Listener {
                     pk.dimensionId = displayEntity.level.getDimensionData().getDimensionId();
                     pk.position = r2;
                     int size = c - '0' ;
-                    pk.molangVariablesJson = ("[{\"name\":\"variable.num\",\"value\":{\"type\":\"float\",\"value\":" +
-                            size +
-                            "}}]").describeConstable();
+                    pk.molangVariablesJson = ("[" +
+                            "{\"name\":\"variable.num\",\"value\":{\"type\":\"float\",\"value\":" +size+ "}},"
+                            +
+                            "{\"name\":\"variable.vx\",\"value\":{\"type\":\"float\",\"value\":" +vx+ "}},"
+                            +
+                            "{\"name\":\"variable.vy\",\"value\":{\"type\":\"float\",\"value\":" +vy+ "}},"
+                            +
+                            "{\"name\":\"variable.vz\",\"value\":{\"type\":\"float\",\"value\":" +vz+ "}}"+
+                    "]").describeConstable();
                     ckPlayer.dataPacket(pk);
+
                     v2+= 0.25f;
                 }
             }
@@ -96,6 +126,7 @@ public class PluginMain extends PluginBase implements Listener {
     private Vector3f getSide(Vector3f v3, BlockFace face, double step) {
         return new Vector3f(v3.x + (float)(face.getXOffset() * step), v3.y + (float)(face.getYOffset() * step), v3.z + (float) (face.getZOffset() * step));
     }
+
 
 
 }
